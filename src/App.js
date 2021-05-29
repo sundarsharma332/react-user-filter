e17f279357eb125a2b599488c4eb442a2841b15d
Sundar Raj sharma <50244255+sundarsharma332@users.noreply.github.com>
2021-05-29 23:59:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+import $ from 'jquery';
 import './App.css';
-
 function App() {
+  //const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState(null);
+
+  useEffect(async () => {
+    const response = await fetch("https://reqres.in/api/users?delay=3");
+    const info = await response.json();
+    console.clear();
+    setDetails(info.data);
+    setLoading(false);
+  }, []);
+
+  $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#userinfo__id .p").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {loading ?
+        <div className="loader__container">
+          <h1 className="preloader__text">Loading</h1>
+          <div className="loader">
+
+          </div>
+        </div>
+        :
+        <div className="container">
+         <div className="user__headline">
+           <h1>User Data</h1>
+         </div>
+          <input type="text" className="form-control search__input" id="myInput" placeholder="search here" />
+          <div className="container">
+            {details.map((user) => (
+              <div className ="simple__user" id = "userinfo__id">
+                <p>First Name: {user.first_name}</p>
+                </div>
+            ))};
+          </div>
+        </div>
+      }
     </div>
   );
 }
